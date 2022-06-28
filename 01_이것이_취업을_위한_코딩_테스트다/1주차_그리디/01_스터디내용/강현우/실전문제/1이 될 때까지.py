165d47b49ec48fc3b29423681b138e264f3b2329
kanghw7730 <kanghw7730@gmail.com>
2022-06-28 22:08:32 +0900

    [ 이것이 취업을 위한 코딩 테스트다 : 강현우 ] 01. 그리디

--- 01_이것이_취업을_위한_코딩_테스트다/1주차_그리디/01_스터디내용/강현우/실전문제/1이 될 때까지.py
+++ 01_이것이_취업을_위한_코딩_테스트다/1주차_그리디/01_스터디내용/강현우/실전문제/1이 될 때까지.py
@@ -6,6 +6,9 @@ for y in range(1,parameter):
     if k**y <= n: # 미세하게나마 나눌셈보다는 곱셈이 더 연산 속도가 빠른걸로 알아서, 곱셈 적용.
         acount += 1 
 
-result = n-k**acount + acount # n-k**acout가 1번 조건(-1씩 하는 조건)의 총 횟수, acount가 나누는 횟수.
+if acount == 0:
+    result = n
+else: 
+    result = n-k**acount + acount # n-k**acout가 1번 조건(-1씩 하는 조건)의 총 횟수, acount가 나누는 횟수.
 
 print(result)
