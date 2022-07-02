42267cab140d932fa5a905608f17043a9dbe4cc9
0417taehyun <0417taehyun@gmail.com>
2022-07-03 01:48:35 +0900

    [ 이것이 취업을 위한 코딩 테스트다 : 이태현 ] 02. 구현

---
+++ 01_이것이_취업을_위한_코딩_테스트다/2주차_구현/01_스터디내용/이태현/03_알고리즘 유형별 기출문제/02. 문자열 재정렬.py
@@ -0,0 +1,30 @@
+def solution() -> None:
+    S: str = input()
+
+    only_characters: str = ""
+    cumulative_number: int = 0
+    for character in S:
+        if character.isnumeric():
+            cumulative_number += int(character)
+
+        else:
+            only_characters += character
+    
+    answer = "".join(sorted(only_characters)) + str(cumulative_number)
+    print(answer)
+
+
+if __name__ == "__main__":
+    from io import StringIO
+    from unittest.mock import patch
+
+    
+    def test_example_case() -> None:
+        with patch("builtins.input", side_effect=["K1KA5CB7"]):
+            with patch("sys.stdout", new_callable=StringIO) as test_stdout:
+                solution()
+
+            assert test_stdout.getvalue() == "ABCKK13\n"
+    
+
+    test_example_case()
